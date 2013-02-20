63f2656847bad040c77282b5a335c1d788f7fb53
Kenny Debrauwer <kenny.debrauwer@kunstmaan.be>
2013-02-20 17:06:13 +0100

    admin menu tweaks

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -29,17 +29,21 @@ class AppKernel extends Kernel
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
         }
 
-                $bundles[] = new Kunstmaan\UtilitiesBundle\KunstmaanUtilitiesBundle();
-        // KunstmaanAdminBundle
-        $bundles[] = new FOS\UserBundle\FOSUserBundle();
-        $bundles[] = new Knp\Bundle\MenuBundle\KnpMenuBundle();
-        $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
-        $bundles[] = new Kunstmaan\AdminBundle\KunstmaanAdminBundle();
+        $bundles[] = new Kunstmaan\UtilitiesBundle\KunstmaanUtilitiesBundle();
+        // KunstmaanNodeBundle
+        $bundles[] = new Kunstmaan\NodeBundle\KunstmaanNodeBundle();
+        $bundles[] = new Kunstmaan\SeoBundle\KunstmaanSeoBundle();
+        $bundles[] = new Symfony\Cmf\Bundle\RoutingExtraBundle\SymfonyCmfRoutingExtraBundle();
         // KunstmaanMediaBundle
         $bundles[] = new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle();
         $bundles[] = new Liip\ImagineBundle\LiipImagineBundle();
         $bundles[] = new Knp\Bundle\GaufretteBundle\KnpGaufretteBundle();
         $bundles[] = new Kunstmaan\MediaBundle\KunstmaanMediaBundle();
+        // KunstmaanAdminBundle
+        $bundles[] = new FOS\UserBundle\FOSUserBundle();
+        $bundles[] = new Knp\Bundle\MenuBundle\KnpMenuBundle();
+        $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
+        $bundles[] = new Kunstmaan\AdminBundle\KunstmaanAdminBundle();
         // KunstmaanPagePartBundle
         $bundles[] = new Kunstmaan\PagePartBundle\KunstmaanPagePartBundle();
         // KunstmaanMediaPagePartBundle
@@ -49,10 +53,7 @@ class AppKernel extends Kernel
         // KunstmaanAdminListBundle
         $bundles[] = new Kunstmaan\AdminListBundle\KunstmaanAdminListBundle();
         $bundles[] = new WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle();
-        // KunstmaanNodeBundle
-        $bundles[] = new Kunstmaan\NodeBundle\KunstmaanNodeBundle();
-        $bundles[] = new Kunstmaan\SeoBundle\KunstmaanSeoBundle();
-        $bundles[] = new Symfony\Cmf\Bundle\RoutingExtraBundle\SymfonyCmfRoutingExtraBundle();
+
         // KunstmaanGeneratorBundle
         $bundles[] = new Kunstmaan\GeneratorBundle\KunstmaanGeneratorBundle();
         // KunstmaanNewRelicBundle
