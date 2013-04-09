d744d34dd2ef244f587d8aa0d92fcf8c4e6748e1
Kenny Debrauwer <kenny.debrauwer@kunstmaan.be>
2013-04-09 17:57:33 +0200

    search update

--- src/Kadeke/WebsiteBundle/Entity/Search/SearchPage.php
+++ src/Kadeke/WebsiteBundle/Entity/Search/SearchPage.php
@@ -6,6 +6,7 @@ use Kunstmaan\NodeBundle\Entity\AbstractPage;
 use Doctrine\ORM\Mapping as ORM;
 use Kunstmaan\NodeBundle\Helper\RenderContext;
 use Kunstmaan\SearchBundle\Helper\IndexControllerInterface;
+use Sherlock\Sherlock;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -30,16 +31,58 @@ class SearchPage extends AbstractPage implements IndexControllerInterface {
             }
         }
         if ($querystring and $querystring != "") {
-            $search = $container->get('kunstmaan_search.search');
-            $response = $search->search($querystring, $querytype, $tags);
+            $responseData = $this->search($container, $querystring, $querytype, $tags);
 
-            $context['hits'] = $response->hits;
+            $context['hits'] = $responseData['hits']['hits'];
             $context['q_query'] = $querystring;
             $context['q_tags'] = implode(',', $tags);
             $context['s_tags'] = $tags;
             $context['q_type'] = $querytype;
-            $context['facets'] = $response->responseData['facets'];
+            $context['facets'] = $responseData['facets'];
+            $context['responseDate'] = $responseData;
         }
+    }
+
+    public function search($container, $querystring, $type, $tags)
+    {
+        $search = $container->get('kunstmaan_search.search');
+        $sherlock = $container->get('kunstmaan_search.searchprovider.sherlock');
+        $request = $sherlock->getSherlock()->search();
+
+        $titleQuery = Sherlock::queryBuilder()->Wildcard()->field("title")->value($querystring);
+        $contentQuery = Sherlock::queryBuilder()->Wildcard()->field("content")->value($querystring);
+
+        $query = $tagQuery = Sherlock::queryBuilder()->Bool()->should($titleQuery, $contentQuery)->minimum_number_should_match(1);
+
+        if (count($tags) > 0) {
+            $tagQueries = array();
+            foreach ($tags as $tag) {
+                $tagQueries[] = Sherlock::queryBuilder()->Term()->field("tags")->term($tag);
+            }
+            $tagQuery = Sherlock::queryBuilder()->Bool()->must($tagQueries)->minimum_number_should_match(1);
+            $query = Sherlock::queryBuilder()->Bool()->must(array($tagQuery, $query))->minimum_number_should_match(1);
+        }
+
+        if ($type && $type != '') {
+            $typeQuery = Sherlock::queryBuilder()->Term()->field("type")->term($type);
+            $query = Sherlock::queryBuilder()->Bool()->must(array($typeQuery, $query))->minimum_number_should_match(1);
+        }
+
+        $request->query($query);
+
+        $tagFacet = Sherlock::facetBuilder()->Terms()->fields("tags")->facetname("tag");
+        $typeFacet = Sherlock::facetBuilder()->Terms()->fields("type")->facetname("type");
+        $request->facets($tagFacet, $typeFacet);
+
+        $highlight = Sherlock::highlightBuilder()->Highlight()->pre_tags(array("<strong>"))->post_tags(array("</strong>"))->fields(array("content" => array("fragment_size" => 150, "number_of_fragments" => 1)));
+
+        $request->highlight($highlight);
+
+        $json = $request->toJSON();
+
+        $response = $search->search("nodeindex", "page", $json, true);
+
+        return $response;
     }
 
     /**
